ad2144354901e0564c2e259b7685a9680431a665
Clément DOUIN <clementdouin21@gmail.com>
2016-07-04 17:19:35 +0200

    Updated video media queries + add collapse menu

--- source/themes/soywod/runetsense/assets/js/local.min.js
+++ source/themes/soywod/runetsense/assets/js/local.min.js
@@ -1,14 +1,35 @@
 const SCROLL_TOP_THRESHOLD = 30;
 
 $(function () {
-	var $menu = $(".ui.menu");
+	var $menu               = $(".ui.menu");
+	var $homeHeaders        = $(".home .header");
+	var $homeHeaderIcons    = $(".home .icon");
+	var $homeHeaderContents = $(".home .content");
 
 	$(document).scroll(function (event) {
-		if ($(this).scrollTop() < SCROLL_TOP_THRESHOLD  && $menu.hasClass("scrolled")) {
+		if ($(this).scrollTop() < SCROLL_TOP_THRESHOLD && $menu.hasClass("scrolled")) {
 			$menu.removeClass("scrolled");
 		}
 		else if ($(this).scrollTop() >= SCROLL_TOP_THRESHOLD && ! $menu.hasClass("scrolled")) {
 			$menu.addClass("scrolled");
 		}
-	})
+	});
+
+	$homeHeaders.on({
+		mouseenter: function () {
+			$homeHeaderIcons.css("opacity", .95);
+			$homeHeaderContents.css("opacity", .95);
+		},
+		mouseleave: function () {
+			$homeHeaderIcons.css("opacity", .5);
+			$homeHeaderContents.css("opacity", 0);
+		}
+	});
+
+	$('.right.menu.open').on("click", function (e) {
+		e.preventDefault();
+		$('.ui.vertical.menu').toggle();
+	});
+
+	$('.ui.dropdown').dropdown();
 });
